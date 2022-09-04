e468447443c2ee00664da5b8e88b82b997909706
InterVivos <112789674+InterVivos@users.noreply.github.com>
2022-09-04 03:14:09 -0500

    base para vistas y modelos en django

--- main/views.py
+++ main/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from .models import (
+    UserProfile,
+    Blog,
+)
+from django.views import generic
 # Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        blogs = Blog.objects.filter(is_active = True)
+        
+        context["blogs"] = blogs
+
+        return context
+
+class BlogView(generic.ListView):
+    model = Blog
+    template_name = "main/blog.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active = True)
+
+class BlogDetailView(generic.DetailView):
+    model = Blog
+    template_name = "main/blog-detail.html"
